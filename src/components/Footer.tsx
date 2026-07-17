be5cc26b0c67cb38227693523a2a84e3e9416bcc
AshuSangwan <123232501+AshuSangwan@users.noreply.github.com>
2026-07-17 18:07:16 +0530

    Update Footer.tsx
    .

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -206,7 +206,7 @@ const Footer = () => {
 
         {/* Copyright */}
         <div className="pt-8 text-center">
-          <Paragraph sm white className="opacity-80">
+          <Paragraph sm white className="opacity-80 text-center">
             © {year} WiQi. SamPro Holding Limited.
           </Paragraph>
           {/* <Paragraph sm white className="opacity-80">Developed by  <Link href={"https://delaine.tech/"} target='_blank' className='hover:text-pink! hover:opacity-100'>Delaine</Link></Paragraph> */}
