802e785d6516ee4ae94360e6c839146a0fe1ee7a
AshuSangwan <123232501+AshuSangwan@users.noreply.github.com>
2026-07-17 18:01:38 +0530

    Update layout.tsx
    Layout WiQi chnages

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Wiqi - All Your Entertainment & Communication In One Place",
+  title: "WiQi - All Your Entertainment & Communication In One Place",
   description: "Experience the future of entertainment with Wiqi. Movies, games, chats, music, live streams, calls, and smart features in one ecosystem.",
   keywords: "wiqi, super app, entertainment, communication, streaming, gaming, chat, music",
 };
